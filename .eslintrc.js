729e77d917153b08c5fd120f9e0ae76b25db08c4
thyeone <thyeonee@gmail.com>
2024-04-02 16:49:28 +0900

    chore: 린트 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
   root: true,
   extends: ['@react-native', 'prettier/prettier'],
   rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
     'react/react-in-jsx-scope': 'off',
     'react-native/no-inline-styles': 0,
+    curly: 'off',
+    'no-fallthrough': 'off',
+    'react-hooks/exhaustive-deps': 'off',
   },
 };
